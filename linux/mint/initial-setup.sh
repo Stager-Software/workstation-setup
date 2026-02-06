d8ccad8fefc28b8e45dfd7b8276080132cc30469
Girts Dvinskis <girtys0898@gmail.com>
2026-02-06 13:07:33 +0100

    Auto yes to apt prompts when installing docker

--- linux/mint/initial-setup.sh
+++ linux/mint/initial-setup.sh
@@ -128,7 +128,7 @@ Signed-By: /etc/apt/keyrings/docker.asc
 EOF
 
 sudo apt update
-sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 info "Docker and Docker Compose installed. Setting up permissions..."
 if ! getent group docker > /dev/null; then
